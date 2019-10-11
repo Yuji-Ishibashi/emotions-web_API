fa27e1346cd4756e2725377b008c91da8255f2df
Yuji-Ishibahi <yu.job96@gmail.com>
2019-10-12 02:50:16 +0900

    add config.js login.js

---
+++ js/login.js
@@ -0,0 +1,17 @@
+var vm = new Vue({
+	el: "#app", //Vue.jsを使うタグのIDを指定
+	data: {
+		// Vue.jsで使う変数はここに記載する
+		mode: "login"
+	},
+	methods: {
+		// Vue.jsで使う関数はここで記述する
+	},
+	created: function(){
+		// Vue.jsの読み込みが完了したときに実行する処理はここで記載する
+	},
+	computed: {
+		// 計算した結果を変数として利用したいときはここに記載する
+
+	}
+})

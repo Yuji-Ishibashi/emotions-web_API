c2d522995226b8dad6cd9c2b9285390da61fbf5c
Yuji-Ishibahi <yu.job96@gmail.com>
2019-10-12 03:57:55 +0900

    add post-request-template.js

---
+++ js/post-request-template.js
@@ -0,0 +1,24 @@
+// APIにGETリクエストを送る
+fetch(url + "/test/exho", {
+	method: "POST",
+	body: JSON.stringify({
+		key1: "value1",
+		key2: "value2",
+		key3: "value3"
+	  })
+	})
+	.then(function(response) {
+		if (response.status == 200) {
+			return response.json();
+		}
+		// 200番以外のレスポンスはエラーを投げる
+		return response.json().then(function(json) {
+			throw new Error(json.message);
+		});
+	})
+	.then(function(json){
+		// レスポンスが200で帰ってきたときの処理はここに記載する
+	})
+	.catch(function(err){
+		// レスポンスがエラーで返ってきたときの処理はここで記載する
+	})
